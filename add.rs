f3104c11ff8175babc3d98e333ab5d9a2ea3002d
Simon Larsén <slarse@slar.se>
2022-07-29 10:26:36 +0200

    Implement incremental add to index

--- add.rs
+++ add.rs
@@ -6,15 +6,23 @@ use crate::{
 };
 
 pub fn add(path: PathBuf) -> io::Result<()> {
-    let mut file = File::open(&path)?;
-    let mut bytes: Vec<u8> = Vec::new();
-    file.read_to_end(&mut bytes)?;
-
-    let blob = Blob::new(bytes);
+    let file_bytes = read_file(&path)?;
+    let blob = Blob::new(file_bytes);
     let entry = IndexEntry::new(path, blob.id())?;
 
-    let mut index = Index::new();
+    let index_bytes = read_file(&PathBuf::from(".git/index"))?;
+
+    // TODO handle error from reading index
+    let mut index = Index::from_bytes(&index_bytes).ok().unwrap();
+
     index.add_entry(entry);
 
     fs::write(".git/index", index.as_vec())
+}
+
+fn read_file(path: &PathBuf) -> io::Result<Vec<u8>> {
+    let mut file = File::open(path)?;
+    let mut bytes: Vec<u8> = Vec::new();
+    file.read_to_end(&mut bytes)?;
+    Ok(bytes)
 }
