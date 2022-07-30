68c654b74948f54176fd5f3d53775f93e3412516
Simon Larsén <slarse@slar.se>
2022-07-30 10:12:58 +0200

    Use lockfile to write to index for rm command

--- src/rm.rs
+++ src/rm.rs
@@ -1,15 +1,17 @@
 use std::path::PathBuf;
-use std::{fs, io};
+use std::io;
+use crate::file::LockFile;
 
 use crate::{index::Index, workspace::Workspace};
 
 pub fn rm(path: &PathBuf, workspace: &Workspace) -> io::Result<()> {
-    let index_file = workspace.git_dir().join("index");
-    let mut index = Index::from_file(&index_file)?;
+    let index_file_path = workspace.git_dir().join("index");
+    let mut index_lockfile = LockFile::acquire(&index_file_path)?;
+    let mut index = Index::from_file(&index_file_path)?;
 
     let absolute_path = workspace.workdir().join(&path);
     let relative_path = workspace.relativize_path(&absolute_path);
     index.remove(&relative_path);
 
-    fs::write(&index_file, index.as_vec())
+    index_lockfile.write(&index.as_vec())
 }
