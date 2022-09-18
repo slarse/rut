8fe56f88be47d1b8ff362df1dd6e578f37b76fd1
Simon Larsén <slarse@slar.se>
2022-09-18 17:26:35 +0200

    Return an error when non-existing path is specified

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::io::Error;
+
 use crate::{
     add, commit, init, rm,
     workspace::{Database, Workspace},
@@ -22,13 +24,23 @@ pub fn run_command(args: Vec<String>) -> io::Result<()> {
             commit::commit(&workspace, &database)?;
         }
         ["add", path] => {
-            add::add(PathBuf::from(path), &workspace, &database)?;
+            add::add(resolve_path(path)?, &workspace, &database)?;
         }
         ["rm", path] => {
-            rm::rm(&PathBuf::from(path), &workspace)?;
+            rm::rm(resolve_path(path)?, &workspace)?;
         }
         _ => panic!("unexpected command {:?}", sliced_args),
     };
 
     Ok(())
+}
+
+fn resolve_path(path: &str) -> io::Result<PathBuf> {
+    let resolved = PathBuf::from(path);
+    return if resolved.exists() {
+        Ok(resolved)
+    } else {
+        let message = format!("pathspec {:?} did not match any files", resolved);
+        Err(Error::new(io::ErrorKind::Other, message))
+    }
 }
