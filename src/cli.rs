911ba9b8ff3767e6985e4263b557fc00d390a94b
Simon Larsén <slarse@slar.se>
2023-03-22 23:20:49 +0200

    Use clap for the CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::io::Error;
 
 use crate::diff;
@@ -7,50 +8,74 @@ use std::env;
 use std::io;
 use std::path::PathBuf;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    Init,
+    Commit,
+    Add {
+        path: String,
+    },
+    Remove {
+        path: String,
+    },
+    Status {
+        #[clap(long)]
+        porcelain: bool,
+    },
+    Diff {
+        #[clap(long)]
+        cached: bool,
+    },
+}
+
 pub fn run_command(args: Vec<String>) -> io::Result<()> {
-    let sliced_args: Vec<&str> = args.iter().skip(1).map(|s| s.as_str()).collect();
     let workdir = env::current_dir()?;
     let git_dir = workdir.join(".git");
 
     let repository = Repository::from_worktree_root(workdir);
     let mut writer = StdoutWriter {};
 
-    match sliced_args[..] {
-        ["init"] => {
+    let args = Args::parse_from(args);
+
+    match args.action {
+        Action::Init => {
             init::init(&git_dir, &mut writer)?;
         }
-        ["commit"] => {
+        Action::Commit => {
             commit::commit(&repository, &mut writer)?;
         }
-        ["add", path] => {
-            add::add(resolve_path(path)?, &repository)?;
+        Action::Add { path } => {
+            add::add(resolve_path(&path)?, &repository)?;
         }
-        ["rm", path] => {
-            rm::rm(resolve_path(path)?, &repository)?;
+        Action::Remove { path } => {
+            rm::rm(resolve_path(&path)?, &repository)?;
         }
-        ["status"] => {
-            let status_options = Default::default();
-            status::status(&repository, &status_options, &mut writer)?;
-        }
-        ["status", "--porcelain"] => {
+        Action::Status { porcelain } => {
             let status_options = status::Options {
-                output_format: status::OutputFormat::Porcelain,
+                output_format: if porcelain {
+                    status::OutputFormat::Porcelain
+                } else {
+                    status::OutputFormat::HumanReadable
+                },
             };
             status::status(&repository, &status_options, &mut writer)?;
         }
-        ["diff"] => {
-            let diff_options = Default::default();
-            diff::diff_repository(&repository, &diff_options, &mut writer)?;
-        }
-        ["diff", "--cached"] => {
+        Action::Diff { cached } => {
             let diff_options = diff::OptionsBuilder::default()
-                .cached(true)
+                .cached(cached)
                 .build()
                 .unwrap();
             diff::diff_repository(&repository, &diff_options, &mut writer)?;
         }
-        _ => panic!("unexpected command {:?}", sliced_args),
-    };
+    }
 
     Ok(())
 }
